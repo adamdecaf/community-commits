45e196ddc19ab749ab70973e64b1122342eb18a3
Adam Shannon <adamkshannon@gmail.com>
2024-05-24 09:55:51 -0500

    tracker: one sentence overview

---
+++ internal/tracker/tracker.go
@@ -0,0 +1,9 @@
+package tracker
+
+// TODO(adam):
+//
+// Grab repository and forks, traverse each branch collecting commits
+// dedup commits, but grab new commits, show if they're ahead of mainline,
+// can be merged, how big they are, summary, etc
+//
+// Web UI with button to cherry-pick commit into PR on the mainline.
