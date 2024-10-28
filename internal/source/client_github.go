6afdc21dfe132862db3c0af1d246a0f557bf5a94
Adam Shannon <adamkshannon@gmail.com>
2024-10-28 14:31:17 -0500

    source: pull more history

--- internal/source/client_github.go
+++ internal/source/client_github.go
@@ -66,7 +66,7 @@ func (g *githubSource) ListNetworkPushEvents(ctx context.Context, repo Repositor
 
 	currentListOptions := &github.ListOptions{}
 	maxListOptions := &github.ListOptions{
-		Page:    10,
+		Page:    25,
 		PerPage: 100,
 	}
 	err := g.listNetworkEvents(ctx, repo, collector, currentListOptions, maxListOptions)
